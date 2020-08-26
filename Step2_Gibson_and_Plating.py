a0b3269c9fba7c41a4ec1cea45406e7d1a395ac4
ljm176 <43517811+ljm176@users.noreply.github.com>
2020-08-26 14:23:47 +0200

    Update gibson reaction setup and add transformation step.

--- Step2_Gibson_and_Plating.py
+++ Step2_Gibson_and_Plating.py
@@ -12,6 +12,8 @@ metadata = {
     'apiLevel': '2.2'
 }
 
+
+
 def run(protocol): 
     """
     Sets up and incubates a Gibson Reaction
@@ -20,11 +22,59 @@ def run(protocol):
     tips20= [protocol.load_labware('opentrons_96_tiprack_300ul', '1')]
     tips200 = [protocol.load_labware('opentrons_96_tiprack_300ul', '2')]
     
+    #Load Pipettes
+    p20Single = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=tips20)
+    p300Single = protocol.load_instrument('p300_single', 'left', tip_racks=tips200)
+    
     #Load temperature module
+    temperature_module = protocol.load_module('temperature module', 3)
+    temp_rack = temperature_module.load_labware("opentrons_24_aluminumblock_nest_1.5ml_snapcap")
+    temperature_module.set_temperature(50)
+    
+    
+    #Load Labware
+    pcrPlate = protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '5')
+    masterMixRack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '4')
+    gibsonMasterMix = masterMixRack.wells_by_name()["A1"]
+    water = masterMixRack.wells_by_name()["B1"]
+    transformationPlate = protocol.load_labware("usascientific_96_wellplate_2.4ml_deep", "6")
+    rack_50mL = protocol.load_labware("opentrons_6_tuberack_falcon_50ml_conical", "9")
+    abayli = rack_50mL.wells_by_name()["A1"]
+    
+    
+    #Fragment setups need to be hardcoded. Each nested list should describe the location of 
+    #fragments that will generate a construct
+    fragments = [[pcrPlate.wells()[0], pcrPlate.wells()[1]], 
+                 [pcrPlate.wells()[2], pcrPlate.wells()[3]]
+                 ]
+    
+    def make_gibson(frags, dest):
+        """
+        Makes a 20 uL gibson reaction with 2 fragments and water or 3 fragments.
+        """
+        
+        for f in frags:
+            p20Single.transfer(3.3, f, dest, new_tip="always")
+        if len(frags) == 2:
+            p20Single.transfer(3.3, water, dest, new_tip="always")
+        p20Single.transfer(10, gibsonMasterMix, dest, new_tip="always", mix_after=(3, 20))
 
-     temperature_module = protocol.load_module('temperature module', 3)
-     temperaure_module.load_labware("opentrons_24_aluminumblock_nest_1.5ml_snapcap")
-     
-     temperature_module.set_temp(50)
-     
-     
+    w=0
+    for x in fragments:
+        make_gibson(x, temp_rack.wells()[w])
+        
+    #Incubation
+    protocol.delay(minutes = 120)
+    
+    protocol.comment("Add overnight culture to A1 on rack at position 9,\
+                     and deep well plate preloaded with 450uL LB to position 6")
+    
+    p300Single.distribute(50, abayli, transformationPlate.wells()[0:len(fragments)])
+    
+    
+    
+    
+    
+    
+    
+    
