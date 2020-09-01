bd5af724073e9c34da32a4308fa63bbaaa92626a
ljm176 <43517811+ljm176@users.noreply.github.com>
2020-09-01 14:25:24 +0200

    Rewrite to plate out in 3 concentrations.

--- Step3_plating.py
+++ Step3_plating.py
@@ -12,6 +12,7 @@ metadata = {
     'apiLevel': '2.2'
 }
 
+nConstructs = 6
 
 def run(protocol):
     
@@ -24,45 +25,57 @@ def run(protocol):
     p300Single = protocol.load_instrument('p300_single', 'left', tip_racks=tips200)
     
     #load labware
+    #Transformation wellplates
+    transformation = protocol.load_labware('usascientific_96_wellplate_2.4ml_deep', '7')
+    dilution1 = protocol.load_labware('usascientific_96_wellplate_2.4ml_deep', '8')
+    dilution2 = protocol.load_labware('usascientific_96_wellplate_2.4ml_deep', '9')
     
-    transformation = protocol.load_labware('usascientific_96_wellplate_2.4ml_deep', '4')
-    dilution = protocol.load_labware('usascientific_96_wellplate_2.4ml_deep', '5')
-    agar = protocol.load_labware("corning_24_wellplate_3.4ml_flat", "6")
-    lb_rack = protocol.load_labware("opentrons_6_tuberack_falcon_50ml_conical", "7")
+    #Agar plates
+    
+    agar1 = protocol.load_labware("corning_24_wellplate_3.4ml_flat", "4")
+    agar2 = protocol.load_labware("corning_24_wellplate_3.4ml_flat", "5")
+    agar3 = protocol.load_labware("corning_24_wellplate_3.4ml_flat", "6")
+    
+    
+    lb_rack = protocol.load_labware("opentrons_6_tuberack_falcon_50ml_conical", "10")
     lb = lb_rack.wells_by_name()["A1"]
+    
 
-    def spot(well, spot_vol=10):
+    def spot(dest, spot_vol):
         """Takes a diluted transformed culture and spots 50 microlitres onto agar 
         in a Nunc omnitray"""
-        
-        dil = dilution.wells()[well]
-        dest = agar.wells()[well]
+
 
         SAFE_HEIGHT = 15  # height avoids collision with agar tray.
         spotting_dispense_rate=0.025 
-        
-        p300Single.aspirate(spot_vol, dil)
+
         p300Single.move_to(dest.top(SAFE_HEIGHT))
+        protocol.max_speeds["Z"] = 50
         p300Single.move_to(dest.top(2))
         p300Single.dispense(volume=spot_vol, rate=spotting_dispense_rate)
-        p300Single.move_to(dest.top(1))
-
-
-    def dilute(well):
-        p300Single.consolidate([135, 15], [lb, transformation.wells()[well]], 
-                               dilution.wells()[well], mix_after=(3, 150),
-                               new_tip="never")
+        p300Single.move_to(dest.top(0))
+        del protocol.max_speeds["Z"]
         
     
-    def dilute_then_spot(well):
+    
+    #Load LB into neccesary wells
+    p300Single.distribute(90, lb, [dilution1.wells()[0:nConstructs], dilution2.wells()[0:nConstructs]])
+    
+    def dilute_and_spot(well, spot_vol):
         p300Single.pick_up_tip()
-        dilute(well)
-        spot(well)
+        p300Single.aspirate(10 + spot_vol, transformation.wells()[well])
+        spot(agar1.wells()[well], spot_vol)
+        p300Single.dispense(10, dilution1.wells()[well])
+        p300Single.mix(4, 100, dilution1.wells()[well])
+        p300Single.aspirate(10 + spot_vol, dilution1.wells()[well])
+        spot(agar2.wells()[well], spot_vol)
+        p300Single.dispense(10, dilution2.wells()[well])
+        p300Single.mix(4, 100, dilution2.wells()[well])
+        p300Single.aspirate(10 + spot_vol, dilution2.wells()[well])
+        spot(agar3.wells()[well], spot_vol)
         p300Single.drop_tip()
         
-    for w in list(range(5)):
-        dilute_then_spot(w)
-           
-        
+    for w in list(range(nConstructs)):
+        dilute_and_spot(w, 10)
         
     
