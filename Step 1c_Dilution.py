a32780689666e1f42e1966bef9e031ddacb40d22
ljm176 <43517811+ljm176@users.noreply.github.com>
2020-09-02 11:01:46 +0200

    dpn1 Reaction on Temp module

--- Step 1c_Dilution.py
+++ Step 1c_Dilution.py
@@ -20,14 +20,21 @@ def run(protocol):
     #Load Tips
     tips20= [protocol.load_labware('opentrons_96_tiprack_300ul', '1')]
     tips200 = [protocol.load_labware('opentrons_96_tiprack_300ul', '2')]
+    
+    
 
     #Load Pipettes
     p20Single = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=tips20)
     p300Single = protocol.load_instrument('p300_single', 'left', tip_racks=tips200)
     
+    temp_mod = protocol.load_module("tempdeck", "7")
+    dilutionPlate = temp_mod.load_labware('nest_96_wellplate_100ul_pcr_full_skirt')
+    
+    temp_mod.set_temperature(37)
+    
     #load Labware
     pcrPlate = protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '3')
-    dilutionPlate = protocol.load_labware('nest_96_wellplate_100ul_pcr_full_skirt', '4')
+    
     
     rack= protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '5')
     water = rack.wells_by_name()["A1"]
@@ -40,3 +47,5 @@ def run(protocol):
         p20Single.transfer(15, pcrPlate.wells()[w], dilutionPlate.wells()[w],
                            new_tip = "always", mix_after = (3, 20))
         
+    protocol.delay(minutes=15)
+    temp_mod.set_temperature(4)
