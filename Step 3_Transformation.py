b5e57354c0d6092fce0076e6ec613e9077f91c5a
ljm176 <43517811+ljm176@users.noreply.github.com>
2020-09-03 11:01:26 +0200

    Premix culture and LB

--- Step 3_Transformation.py
+++ Step 3_Transformation.py
@@ -8,7 +8,7 @@ metadata = {
     'apiLevel': '2.2'
 }
 
-nConstructs = 6    
+nConstructs = 8    
 
 def run(protocol):
         
@@ -26,12 +26,11 @@ def run(protocol):
         transformationPlate = protocol.load_labware("usascientific_96_wellplate_2.4ml_deep", "4")
         rack_50mL = protocol.load_labware("opentrons_6_tuberack_falcon_50ml_conical", "9")
         abayli = rack_50mL.wells_by_name()["A1"]
-        lb = rack_50mL.wells_by_name()["B1"]
-        
-        p300Single.distribute(450, lb, transformationPlate.wells()[0:nConstructs])
 
-        #Add overnight culture to LB.
-        p300Single.distribute(50, abayli, transformationPlate.wells()[0:nConstructs])
+        
+        p300Single.distribute(500, abayli, transformationPlate.wells()[0:nConstructs])
+
+
         
         #Trasnfer 
         for i in list(range(0,nConstructs)):
