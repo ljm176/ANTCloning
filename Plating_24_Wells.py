e33484f3f365afed54da1cdd0e1dc1a89d3d980a
Lachlan Munro <43517811+ljm176@users.noreply.github.com>
2021-06-25 13:09:04 +0200

    Update Plating

--- Plating_24_Wells.py
+++ Plating_24_Wells.py
@@ -12,8 +12,8 @@ metadata = {
     'apiLevel': '2.2'
 }
 
-#24 well layout
-transformed = [x + i for x in range(0, 43, 8) for i in range(4)]
+#Filled By columns
+transformed = list(range(33))
 dilution = [i + 48 for i in transformed]
 
 def run(protocol):
@@ -27,7 +27,7 @@ def run(protocol):
     #Load Pipettes
     #p20Single = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=tips20)
     p300Single = protocol.load_instrument('p300_single', 'left', tip_racks=tips200)
-    
+
     #load labware
     #Transformation wellplates
     transformation = protocol.load_labware('usascientific_96_wellplate_2.4ml_deep', '2')
@@ -35,23 +35,25 @@ def run(protocol):
     transformation_wells = [transformation.wells()[i] for i in transformed]
     dilution_wells = [transformation.wells()[i] for i in dilution]
 
+    falconRack = protocol.load_labware("opentrons_6_tuberack_falcon_50ml_conical", 3)
+    saline = falconRack["A1"]
 
-    
+    p300Single.distribute(450, saline, dilution_wells)
+
     #Agar plates
     
     agar1 = protocol.load_labware("corning_24_wellplate_3.4ml_flat", "4")
     agar2 = protocol.load_labware("corning_24_wellplate_3.4ml_flat", "5")
+    agar3 = protocol.load_labware("corning_24_wellplate_3.4ml_flat", "6")
+    agar4 = protocol.load_labware("corning_24_wellplate_3.4ml_flat", "7")
 
 
-    spots = agar1.wells()
-    spots2 = agar2.wells()
-    
-
+    spots = agar1.wells() + agar2.wells()
+    spots2 = agar3.wells() + agar4.wells()
 
     def spot(dest, spot_vol):
         """Takes a diluted transformed culture and spots the defined volume onto agar 
         in a Nunc omnitray"""
-
 
         SAFE_HEIGHT = 15  
         spotting_dispense_rate=0.25 
@@ -64,7 +66,7 @@ def run(protocol):
         del protocol.max_speeds["Z"]
         
 
-    spotVol = 10
+    spotVol = 20
 
     for i in range(24):
         p300Single.pick_up_tip()
